58b5559b847b1bc782ad4843b63c6454306bfedb
Abraham Romero <abrahamciclo@gmail.com>
2020-09-22 18:52:49 +0200

    Realizado los ejercicios 1 y 2 de eventos de js

---
+++ JavaScript/Eventos/Ejercicio Básico 1 y 2/script.js
@@ -0,0 +1,26 @@
+const caja = document.getElementById("box");
+const texto = document.getElementById("text");
+
+caja.addEventListener( "mouseenter", () => {
+    caja.style.background = "green";
+});
+
+caja.addEventListener( "mouseleave", () => {
+    caja.style.background = "red";
+});
+
+caja.addEventListener( "mousedown", () => {
+    console.log("Has pulsado la caja!");
+});
+
+caja.addEventListener( "mouseup", () => {
+    console.log("Has soltado el botón izquierdo dentro de la caja!");
+});
+
+texto.addEventListener( "keydown", () => {
+    console.log("Has pulsado una tecla!");
+});
+
+texto.addEventListener( "keyup", () => {
+    console.log("Has soltado una tecla!");
+});
