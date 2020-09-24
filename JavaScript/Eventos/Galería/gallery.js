3e42cb9eb23246751daca49e949cb86e883a73b5
Abraham Romero <abrahamciclo@gmail.com>
2020-09-24 18:48:15 +0200

    Subiendo ejercicios de Eventos JS

--- JavaScript/Eventos/Galería/gallery.js
+++ JavaScript/Eventos/Galería/gallery.js
@@ -2,6 +2,5 @@ const gallery=document.getElementById("gallery");
 
 //Reemplazamos el color de fondo del boton al hacer click
 gallery.addEventListener("click", (event) => {
-    event.target.classList.replace("gallery-item", "gallery-red");
+        event.target.classList.replace("gallery-item", "gallery-red");
 });
-
