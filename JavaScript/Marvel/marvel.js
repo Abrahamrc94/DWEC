463ae109d35a6d856ecc92dd71c3308253dbeca2
Abraham <abrahamciclo@gmail.com>
2020-10-20 11:05:50 +0200

    Ejercicio 1 marvel

--- JavaScript/Marvel/marvel.js
+++ JavaScript/Marvel/marvel.js
@@ -3,10 +3,10 @@ const select = document.getElementById("select");
 let miLista = document.getElementById('table');
 let datos = document.getElementById("datos");
 
+
 button.addEventListener('click', (e) =>{
     e.preventDefault();
     getData(select.value);
-    console.log(select.value);
 });
 
 
@@ -15,7 +15,6 @@ fetch('http://192.168.33.10/Marvel/marvel.php')
 .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
 .then(res => res.json())
 .then(res => {
-    console.log(res);
         for(superheroe of res){
             let option = document.createElement('option');
             //CREAMOS Y APLICAMOS VALOR AL ATRIBUTO VALUE CON EL ID DE CADA SUPERHEROE
@@ -30,11 +29,11 @@ fetch('http://192.168.33.10/Marvel/marvel.php')
 //CREAMOS FUNCION PARA RECUPERAR DATOS DE LA BBDD
 const getData = (id) => {
             //BUSCAMOS POR ID
+            let datos = document.getElementById("datos");
             fetch(`http://192.168.33.10/Marvel/marvel.php?id=${id}`)
             .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
             .then(res => res.json())
             .then(res => {
-                console.log(res);
                 let miTr = document.createElement('tr');
                 let miTd = document.createElement('td');
                 let miTexto = document.createTextNode(res[0].Name);
@@ -48,6 +47,7 @@ const getData = (id) => {
                 miTexto = document.createTextNode(res[0].Fighting_Skills);
                 miTd.appendChild(miTexto);
                 miTr.appendChild(miTd);
+                miTr.id=("datos");
                 datos.replaceWith(miTr);
             });  
         };
