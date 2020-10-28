531122c9802e6a85481bc1ae6c5f083f069cb0dd
Abraham <abrahamciclo@gmail.com>
2020-10-28 08:14:09 +0100

    Ejercicio webStorage

--- JavaScript/WebStorage/script.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById('form');
-const keys = document.getElementById('keys');
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    sessionStorage.setItem('name', 'Dorian');
-})
