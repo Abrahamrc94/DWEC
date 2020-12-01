1e21a5d1565eb7048610c2f7f5e5fa195070cbea
Abraham <abrahamciclo@gmail.com>
2020-12-01 11:58:16 +0100

    Proyecto covid angular (Casi acabado)

---
+++ Angular/covid/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { GlobalComponent } from './components/global/global.component';
+import { InformationComponent } from './components/information/information.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: GlobalComponent,
+  },
+  {
+    path: 'global',
+    component: GlobalComponent,
+  },
+  {
+    path: 'countries',
+    component: InformationComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
